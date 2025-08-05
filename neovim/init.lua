3b56dd86c30d7d9ce72da8599e9fc76a0d4bb2d5
Martin Bullman <martin.j.bullman@gmail.com>
2025-08-06 00:40:14 +0300

    Refactors init.lua and treesitter.lua plugins to clean code

--- neovim/init.lua
+++ neovim/init.lua
@@ -7,7 +7,7 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({
+    vim.fn.system({
         "git",
         "clone",
         "--filter=blob:none",
--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -10,10 +10,15 @@ return {
     build = ":TSUpdate",
     config = function()
         local config = require("nvim-treesitter.configs")
+
         config.setup({
             auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true }
+            highlight = {
+                enable = true
+            },
+            indent = {
+                enable = true
+            }
         })
     end
 }
