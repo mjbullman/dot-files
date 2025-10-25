07fcd5e693e4a2ba19ed5774d2b55b7818fd735b
Martin Bullman <martin.j.bullman@gmail.com>
2025-10-26 00:31:53 +0300

    Updates Telescope plugin code formatting.

--- neovim/lua/plugins/telescope.lua
+++ neovim/lua/plugins/telescope.lua
@@ -19,14 +19,14 @@ return {
     {
         "nvim-telescope/telescope-ui-select.nvim",
         config = function()
-            require("telescope").setup {
+            require("telescope").setup{
                 extensions = {
                     ["ui-select"] = {
-                        require("telescope.themes").get_dropdown{
-                        }
+                        require("telescope.themes").get_dropdown{}
                     }
                 }
             }
+
             require("telescope").load_extension("ui-select")
         end
     }
