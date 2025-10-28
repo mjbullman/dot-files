5297113702422ae79eb29be6f0b7088f06312720
Martin Bullman <martin.j.bullman@gmail.com>
2025-10-28 07:56:06 +0200

    Updates Neovim telescope plugin configuration

---
+++ neovim/lua/config/telescope-ui.lua
@@ -0,0 +1,14 @@
+-- -----------------------------
+-- Telescope UI Setup
+-- -------------------------------
+local telescope = require("telescope")  
+
+telescope.setup({
+	extensions = {
+		["ui-select"] = {
+			require("telescope.themes").get_dropdown({}),
+		},
+	},
+})
+telescope.load_extension("ui-select")
+
---
+++ neovim/lua/config/telescope.lua
@@ -0,0 +1,22 @@
+-- ===============================
+--  Telescope Plugin Configuration
+--  Author: Martin Bullman
+-- ===============================
+
+local builtin = require("telescope/builtin")
+
+-- -----------------------------
+-- Custom Keymaps for Telescope
+-- -------------------------------
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {
+	desc = "Telescope: Find Files",
+})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {
+	desc = "Telescope: Live Grep",
+})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {
+	desc = "Telescope: Find Buffers",
+})
+vim.keymap.set("n", "<leader>fg", builtin.help_tags, {
+	desc = "Telescope: Help Tags",
+})
