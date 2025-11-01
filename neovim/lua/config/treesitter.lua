648a1de80ba66d7ae2d325a9beae0f3d96a68e79
Martin Bullman <martin.j.bullman@gmail.com>
2025-11-01 22:00:24 +0200

    Updates Treesitter and moves configuration to separate file.

---
+++ neovim/lua/config/treesitter.lua
@@ -0,0 +1,16 @@
+-- ===============================
+-- Treesitter Plugin Configuration
+-- Author: Martin Bullman
+-- ===============================
+
+local config = require("nvim-treesitter.configs")
+
+config.setup({
+	auto_install = true,
+	highlight = {
+		enable = true,
+	},
+	indent = {
+		enable = true,
+	},
+})
--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -9,17 +9,7 @@ return {
     lazy = false,
     build = ":TSUpdate",
     config = function()
-        local config = require("nvim-treesitter.configs")
-
-        config.setup({
-            auto_install = true,
-            highlight = {
-                enable = true
-            },
-            indent = {
-                enable = true
-            }
-        })
+        require("config.treesitter")
     end
 }
 
