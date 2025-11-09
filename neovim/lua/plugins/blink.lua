605e8b897a669fab69ce3d9abfa235942d0310fb
Martin Bullman <martin.j.bullman@gmail.com>
2025-11-10 01:19:00 +0200

    Removed LuaSnip plugin

--- neovim/lua/plugins/blink.lua
+++ neovim/lua/plugins/blink.lua
@@ -7,7 +7,6 @@ return {
     {
         "saghen/blink.cmp",
         dependencies = {
-            "L3MON4D3/LuaSnip",         -- required for snippet expansion
             "rafamadriz/friendly-snippets"
         },
         version = "*",
