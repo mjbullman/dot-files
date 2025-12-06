20a825b31c88ca15aa6403dc1bbed602c2548ce5
Martin Bullman <martin.j.bullman@gmail.com>
2025-12-06 23:09:11 +0200

    Adds Rust LSP config

---
+++ neovim/lsp/rust_ls.lua
@@ -0,0 +1,10 @@
+-- ==============================
+-- Rust LSP Config
+-- Author: Martin Bullman
+-- ==============================
+
+return {
+    cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/rust-analyzer") },
+    root_markers = { "Cargo.toml" },
+    filetypes = { "rust" },
+}
