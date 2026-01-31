f38f2e0cf68eafd817f8452461c89c53c079c027
Martin Bullman <martin.j.bullman@gmail.com>
2026-01-31 16:10:00 +0200

    Updates the lazy.lua to use single quotes and clean up file formating

--- neovim/lua/core/lazy.lua
+++ neovim/lua/core/lazy.lua
@@ -4,14 +4,14 @@
 -- =================================
 
 -- install lazy plugin manager
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
     vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "--branch=stable",
+        'git',
+        'clone',
+        '--filter=blob:none',
+        '--branch=stable',
         lazyrepo,
         lazypath
     })
@@ -20,9 +20,9 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- setup lazy.nvim
-require("lazy").setup({
+require('lazy').setup({
     spec = {
-        { import = "plugins" },
+        { import = 'plugins' },
     },
     checker = {
         enabled = true,
@@ -33,20 +33,20 @@ require("lazy").setup({
     },
 })
 
--- Configure LSP with Blink.cmp capabilities (after lazy loads plugins)
+-- configure LSP with Blink.cmp capabilities (after lazy loads plugins)
 vim.lsp.config('*', {
     capabilities = require('blink.cmp').get_lsp_capabilities(),
     root_markers = { '.git' },
 })
 
--- Configure lua_ls for Neovim development
+-- configure lua_ls for Neovim development
 vim.lsp.config('lua_ls', {
     settings = {
         Lua = {
             runtime = { version = 'LuaJIT' },
             diagnostics = { globals = { 'vim' } },
             workspace = {
-                library = vim.api.nvim_get_runtime_file("", true),
+                library = vim.api.nvim_get_runtime_file('', true),
                 checkThirdParty = false,
             },
             telemetry = { enable = false },
@@ -54,5 +54,16 @@ vim.lsp.config('lua_ls', {
     },
 })
 
--- Enable all LSP servers
-vim.lsp.enable({ "clangd", "lua_ls", "vtsls", "vue_ls", "basedpyright", "html_ls", "css_ls", "eslint_ls", "jdtls", "rust_ls" })
+-- enable all LSP servers
+vim.lsp.enable({
+    'vtsls',
+    'jdtls',
+    'lua_ls',
+    'vue_ls',
+    'clangd',
+    'css_ls',
+    'rust_ls',
+    'html_ls',
+    'eslint_ls',
+    'basedpyright',
+})
