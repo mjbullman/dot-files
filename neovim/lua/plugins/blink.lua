0b096552b7bfec579bd0b8a21037b93454c1820c
Martin Bullman <martin.j.bullman@gmail.com>
2026-02-05 00:23:19 +0200

    Minor Neovim config clean up

--- neovim/lua/plugins/blink.lua
+++ neovim/lua/plugins/blink.lua
@@ -4,16 +4,14 @@
 -- =======================
 
 return {
-    {
-        'saghen/blink.cmp',
-        dependencies = {
-            'rafamadriz/friendly-snippets',
-            'dsznajder/vscode-es7-javascript-react-snippets',  -- Better JS/React
-            'hollowtree/vscode-vue-snippets',                  -- Vue 3 snippets
-        },
-        version = '*',
-        config = function()
-            require('config.blink')
-        end,
+    'saghen/blink.cmp',
+    dependencies = {
+        'rafamadriz/friendly-snippets',
+        'dsznajder/vscode-es7-javascript-react-snippets',  -- Better JS/React
+        'hollowtree/vscode-vue-snippets',                  -- Vue 3 snippets
     },
+    version = '*',
+    config = function()
+        require('config.blink')
+    end,
 }
