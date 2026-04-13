2703fe73de85a3fa8701b0611b875d95adebd6f7
mjbullman <martin.bullman@yester.eu>
2026-04-13 23:39:12 +0300

    Update Neovim Lualine config to fix warning with theme setting

--- neovim/lua/config/lualine.lua
+++ neovim/lua/config/lualine.lua
@@ -6,7 +6,7 @@
 require('lualine').setup({
     options = {
         icons_enabled = true,
-        theme = 'catppuccin',
+        theme = 'catppuccin-mocha',
         component_separators = {
             left = '',
             right = '',
