91dad8e430edf43bd7564cc551e3a222808fddfe
Martin Bullman <martin.j.bullman@gmail.com>
2026-08-30 22:10:47 +0300

    Update dev env script to remove unused variable

--- scripts/dev_env.sh
+++ scripts/dev_env.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 SESSION_NAME="MJBDotFiles"
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 if tmux has-session -t "${SESSION_NAME}" 2>/dev/null; then
     echo "Attaching to existing session: ${SESSION_NAME}..."
